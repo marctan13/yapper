54cf65d4118400e6da234cdc5033e0e8a6dd508c
marc-tan <marctan13@gmail.com>
2024-02-06 14:30:11 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./App.css";
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+function App() {
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
+      </header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAkCDXQdHEhy3v4VTshG0jZGTJF6vVk4B0",
+  authDomain: "yapperchat-2c0f0.firebaseapp.com",
+  projectId: "yapperchat-2c0f0",
+  storageBucket: "yapperchat-2c0f0.appspot.com",
+  messagingSenderId: "795336669832",
+  appId: "1:795336669832:web:9dbe41d04889d31f74fd3d",
+  measurementId: "G-N4763EEYVH",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
